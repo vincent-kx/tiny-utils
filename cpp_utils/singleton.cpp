eb43857ba59207eb6c6b27be87a75e84652f98f0
xin.ke <ilinuxcoder.gmail.com>
2018-04-22 12:27:59 +0800

    单例支持C++11以下版本

--- cpp_utils/singleton.cpp
+++ cpp_utils/singleton.cpp
@@ -1,76 +1,167 @@
 #include <iostream>
-#include <atomic>
+#include "cppversion_macro.h"
 
 using namespace std;
 
-static atomic_flag lock_ = ATOMIC_FLAG_INIT;
+#if(SUPPORT_CPP11)
+	#include <atomic>
 
-template<typename T>
-class Singleton
-{
-public:
-	template<typename ... Args>
-	static T * Instance(Args && ...args)
+	static atomic_flag lock_ = ATOMIC_FLAG_INIT;
+
+	template<typename T>
+	class Singleton
 	{
-		if(instance_ != nullptr)
-			return instance_;
-		else
+	public:
+		template<typename ... Args>
+		static T * Instance(Args && ...args)
 		{
-			while(lock_.test_and_set());//spinlock
-			if(instance_ == nullptr)
-				instance_ = new T(std::forward<Args>(args)...);
-			lock_.clear();
-
+			static SingletonCleaner singleton_cleaner;
+			if(instance_ != nullptr)
+				return instance_;
+			else
+			{
+				while(lock_.test_and_set());//spinlock
+				if(instance_ == nullptr)
+					instance_ = new T(std::forward<Args>(args)...);
+				lock_.clear();
+			}
+			return instance_;
 		}
-		return instance_;
-	}
 
-private:
-	Singleton() = default;
-	Singleton(const Singleton &) = delete;
-	Singleton(Singleton && s);
-	Singleton & operator=(const Singleton &) = delete;
-	~Singleton(){}
-private:
-	static T * instance_;
-};
+	private:
+		Singleton() = default;
+		Singleton(const Singleton &) = delete;
+		Singleton(Singleton && s);
+		Singleton & operator=(const Singleton &) = delete;
+		~Singleton(){}
+	private:
+		static T * instance_;
 
-template<typename T>
-T * Singleton<T>::instance_ = nullptr;
+	private:
+	    class SingletonCleaner
+	    {
+	    public:
+	        ~SingletonCleaner()
+	        {
+	            if (NULL != instance_) delete instance_;
+	            instance_ = NULL;
+	            cout << "Instance destroied" << endl;
+	        }
+	    };
+	};
 
-#if 1
+	template<typename T>
+	T * Singleton<T>::instance_ = nullptr;
 
-#include <thread>
+	#if 1
 
+		#include <thread>
 
-class Test
-{
-public:
-	Test(int ii,double dd):i(ii),d(dd){ cout << "Test constructor" << endl; }
-	~Test(){}
-private:
-	int i;
-	double d;
-};
+		class Test
+		{
+		public:
+			Test(int ii,double dd):i(ii),d(dd){ cout << "Test constructor" << endl; }
+			~Test(){}
+		private:
+			int i;
+			double d;
+		};
 
-void test_func()
-{
-	for(int i = 0; i < 1000; ++i)
+		void test_func()
+		{
+			for(int i = 0; i < 100; ++i)
+			{
+				Test * t = Singleton<Test>::Instance(1,1.1);
+				cout << this_thread::get_id() << "--" << t << endl;
+			}
+		}
+
+		int main(int argc, char const *argv[])
+		{
+			thread t1(test_func);
+			thread t2(test_func);
+			thread t3(test_func);
+			t1.join();
+			t2.join();
+			t3.join();
+			return 0;
+		}
+
+	#endif
+
+#else
+	#include <pthread.h>
+
+	template<typename T>
+	class Singleton
 	{
-		Test * t = Singleton<Test>::Instance(1,1.1);
-		cout << this_thread::get_id() << "--" << t << endl;
-	}
-}
+	public:
+		static T * Instance()
+		{
+			static SingletonCleaner singleton_cleaner;
+			pthread_once(&once,&Singleton::InstanceInit);
+			return instance_;
+		}
+	private:
+		Singleton(){}
+		~Singleton(){}
+		Singleton(const Singleton &){}
+		Singleton & operator=(const Singleton &){}
 
-int main(int argc, char const *argv[])
-{
-	thread t1(test_func);
-	thread t2(test_func);
-	thread t3(test_func);
-	t1.join();
-	t2.join();
-	t3.join();
-	return 0;
-}
+		static void InstanceInit()
+		{
+			instance_ = new T();
+		}
+	private:
+		static T * instance_;
+		static pthread_once_t once;
 
+	private:
+	    class SingletonCleaner
+	    {
+	    public:
+	        ~SingletonCleaner()
+	        {
+	            if (NULL != instance_)
+	            {
+	            	delete instance_;
+	            	instance_ = NULL;
+	            	cout << "Instance destroied" << endl;
+	            }
+	        }
+	    };
+	};
+
+	template<typename T>T * Singleton<T>::instance_ = NULL;
+	template<typename T>pthread_once_t Singleton<T>::once = PTHREAD_ONCE_INIT;	
+
+	#if 1
+		class Test{
+		public:
+			Test(){ cout << "test obj constructor" << endl; }
+		};
+
+		void * test_func(void * tid)
+		{
+			for(int i = 0; i < 100; ++i)
+			{
+				Test * t = Singleton<Test>::Instance();
+				cout << "thread id:" << (long)pthread_self() <<" test obj addr:" << t << endl;
+			}
+		}
+
+		int main(int argc, char const *argv[])
+		{
+			pthread_t thread1;
+			pthread_t thread2;
+			pthread_t thread3;
+			pthread_create(&thread1,NULL,test_func,NULL);
+			pthread_create(&thread2,NULL,test_func,NULL);
+			pthread_create(&thread3,NULL,test_func,NULL);
+			pthread_join(thread1,NULL);
+			pthread_join(thread2,NULL);
+			pthread_join(thread3,NULL);
+			return 0;
+		}
+	#endif
 #endif
